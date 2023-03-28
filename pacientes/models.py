687acd2eaaffb6025522265b990305ef28c27c62
gnassar99 <60226993+gnassar99@users.noreply.github.com>
2023-03-28 12:10:39 -0500

    Atributos

--- pacientes/models.py
+++ pacientes/models.py
@@ -4,11 +4,11 @@ from django.db import models
 class Paciente(models.Model):
     nombre = models.CharField(max_length=100)
     apellido = models.CharField(max_length=100)
-    # edad = models.IntegerField()    
-    # telefono = models.IntegerField()
-    # email = models.CharField(max_length=100)
-    # fechaNacimiento = models.DateField()
-    # triage = models.CharField(max_length=100)    
+    edad = models.IntegerField()    
+    telefono = models.IntegerField()
+    email = models.CharField(max_length=100)
+    fechaNacimiento = models.DateField()
+    triage = models.CharField(max_length=100)    
 
     def __str__(self):
         return self.nombre + ' ' + self.apellido
