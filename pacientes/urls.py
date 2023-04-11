4b80dd384f1bc4e4d6eaf0491ea563c7b2054477
Gabriel Nassar <g.nassar@uniandes.edu.co>
2023-04-11 17:32:14 -0500

    PacientesUrlsSolved

--- pacientes/urls.py
+++ pacientes/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('pacientes/<int:pk>', views.paciente_view, name='paciente_view'),
     path('pacientes/', views.pacientes_view, name='pacientes_view'),
-    # path('', views.pacientes_view, name='pacientes_view'),
-    #path('<int:pk>', views.paciente_view, name='paciente_view'),
+    path('', views.pacientes_view, name='pacientes_view'),
+    path('<int:pk>', views.paciente_view, name='paciente_view'),
 ]
 
