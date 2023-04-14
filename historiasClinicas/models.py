50a8afdb45c137e090cfebd12f26c63df232efd2
Gabriel Nassar <g.nassar@uniandes.edu.co>
2023-04-14 13:15:12 -0500

    JSONFormatArrayFieldChange

--- historiasClinicas/models.py
+++ historiasClinicas/models.py
@@ -1,12 +1,10 @@
 from django.contrib.postgres.fields import ArrayField
 from django.db import models
 from pacientes.models import Paciente
-from jsonfield import JSONField
-
 
 class HistoriaClinica(models.Model):
-    enfermedades = JSONField(default=list)
-    tratamientos = JSONField(default=list)
+    enfermedades = ArrayField(models.CharField(max_length=255))
+    tratamientos = ArrayField(models.CharField(max_length=255))
     paciente = models.OneToOneField(Paciente, on_delete=models.CASCADE, primary_key=True)
 
     def __str__(self):
